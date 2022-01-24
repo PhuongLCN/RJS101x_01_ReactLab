85f1a2c58c05eca86c1c554e920a3d3af370b4bf
Maruko Phuong <lcnphuong@gmail.com>
2022-01-24 22:00:07 +0900

    React Router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import { Component } from 'react';
 import { Navbar, NavbarBrand } from "reactstrap";
 import Main from './components/MainComponent';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
 
   render() {
     return (
-      <div>        
-        <Main />
-      </div>
+      /*Use react router*/
+      <BrowserRouter> 
+        <div>
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
