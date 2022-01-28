302b88b8b615be3cb46630865c0fb83c55d1083c
Maruko Phuong <lcnphuong@gmail.com>
2022-01-28 15:41:46 +0900

    Single Page Applications Exercise 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,11 +5,13 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
+import About from './AboutComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { LEADERS } from '../shared/leaders';
 import { PROMOTIONS } from '../shared/promotions';
 import { Switch, Route, Redirect } from 'react-router-dom';
+
 
 class Main extends Component {
 
@@ -47,7 +49,8 @@ class Main extends Component {
           <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
           <Route path='/menu/:dishId' component={DishWithId} />
           <Route exact path='/contactus' component={Contact} />
-          <Redirect to='/home' />
+          <Route exact path='/aboutus' component={()=><About leaders={this.state.leaders}/>} />
+          <Redirect to='/home' />          
         </Switch>
         <Footer />
       </div>
